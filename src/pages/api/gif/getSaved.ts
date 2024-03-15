eaa4abecfe5f87ea8b575683749060b33e80a56c
Daniil Kulyabov <hi.daniil.k@gmail.com>
2024-03-15 18:32:03 +0300

    add try catch

--- src/pages/api/gif/getSaved.ts
+++ src/pages/api/gif/getSaved.ts
@@ -5,12 +5,20 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { userEmail } = JSON.parse(req.body);
+  try {
+    const { userEmail } = JSON.parse(req.body);
 
-  const { data } = await supabase
-    .from("saved")
-    .select("*")
-    .eq("user_email", userEmail);
+    const { data } = await supabase
+      .from("saved")
+      .select("*")
+      .eq("user_email", userEmail);
 
-  res.status(200).json(data.map((item) => item.gif_id));
+    if (!data) {
+      return res.status(200).json([]);
+    }
+
+    res.status(200).json(data.map((item) => item.gif_id));
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 }
